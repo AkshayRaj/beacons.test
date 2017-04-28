546866e5a1c0fd4d2f3c68d1cd2a552afe208625
akshayrajkore <akshayrajkore@gmail.com>
2017-04-28 00:28:46 -0400

    Added Instrumentation setup for testing BIS SDK

--- app/src/androidTest/java/com/ark/beacons_test/MockTestRunner.java
+++ app/src/androidTest/java/com/ark/beacons_test/MockTestRunner.java
@@ -1,8 +1,12 @@
 package com.ark.beacons_test;
 
+import android.app.Application;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.runner.AndroidJUnitRunner;
+
+import com.beaconsinspace.android.beacon.detector.BISDetector;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,12 +19,12 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+public class MockTestRunner extends AndroidJUnitRunner {
 
-        assertEquals("com.ark.beacons_test", appContext.getPackageName());
+    @Test
+    public void dummyTest(){
+        boolean dummy = true;
+        assertTrue (dummy);
     }
+
 }
